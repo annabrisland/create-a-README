292fd06c7f31156d4359009adda54b2d788767b9
annabrisland <84166964+annabrisland@users.noreply.github.com>
2023-12-16 13:49:43 +0000

    Updates README template

--- README.md
+++ README.md
@@ -1,16 +1,22 @@
-# f
-  f
+![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)
+  # 
+  
   ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
   ## Installation
-  f
+  
   ## Usage
-  f
   
   ## Contributing
-  f
+  
   ## Tests
-  f
+  
   ## Questions
-  f
-  f
-
+  Direct any questions to [](https://github.com/) or email at .
+  ## License
+This project is licensed under the APACHE 2.0 license.
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,24 +1,44 @@
+// Map for license urls
+const urls = new Map([
+  ["MIT", "https://img.shields.io/badge/License-MIT-yellow.svg"],
+  ["APACHE 2.0", "https://img.shields.io/badge/License-Apache_2.0-blue.svg"],
+  ["GPL 3.0", "https://img.shields.io/badge/License-GPLv3-blue.svg"],
+  ["BSD 3", "https://img.shields.io/badge/License-BSD_3--Clause-blue.svg"],
+]);
+
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${
+    data.license != "None"
+      ? `![License](${urls.get(data.license)})`
+      : ""
+  }
   ${data.description}
   ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
   ## Installation
   ${data.installation}
   ## Usage
   ${data.usage}
-  ${data.license != "None" ? 
-    `## License
-    ${data.license}`: ""
-  }
   ## Contributing
   ${data.contributing}
   ## Tests
   ${data.tests}
   ## Questions
-  ${data.username}
-  ${data.email}
-
+  Direct any questions to [${data.username}](https://github.com/${
+    data.username
+  }) or email at ${data.email}.
+  ${
+    data.license != "None"
+      ? `## License\nThis project is licensed under the ${data.license} license.`
+      : ""
+  }
 `;
 }
 
