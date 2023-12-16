e8ce65f21b7dfa6c3401abcc544fca11b66b126a
annabrisland <84166964+annabrisland@users.noreply.github.com>
2023-12-16 09:42:47 +0000

    Adds initial questions

--- index.js
+++ index.js
@@ -5,8 +5,18 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    "What is the title of your project?",
+    "Provide a table of contents.",
+    "What are the installation instructions?",
+    "What are the usage instructions?",
+    "What is the license for this repo?",
+    "Who are the contributors?",
+    "What are the tests for this repo?",
+    "What are the questions for this repo?"
 ];
+
+const [ title, contents, installation, usage, license, contributors, tests, repoQuestions ] = questions;
+
 
 // function to write README file
 function writeToFile(fileName, data) {
